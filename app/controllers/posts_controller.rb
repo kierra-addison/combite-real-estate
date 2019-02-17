27e66355bc2db1ce1d41b518f3c046baacf1144e
kierra-addison <vinediezca@gmail.com>
2019-02-17 13:35:14 +0800

    active storage title_image attachment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,6 +41,8 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    @post.title_image.attach(params[:title_image])
+
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
