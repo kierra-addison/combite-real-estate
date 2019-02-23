99ad16a1bc1ad1431b51c9af9eaf1f3744ef7c18
kierra-addison <vinediezca@gmail.com>
2019-02-23 15:10:26 +0800

    Admin user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+              first_name: 'admin', 
+              last_name: 'admin', 
+              username: 'admin', 
+              password: '1234567890', 
+              password_confirmation: '1234567890', 
+              role: 'test'
+            )
