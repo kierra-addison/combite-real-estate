80d55f8e1966fb847e9ed6aa486b00fd68793d7a
kierra-addison <vinediezca@gmail.com>
2019-06-07 22:13:17 +0800

    covert post's attributes name and description to uppercase before saving

--- app/models/post_attribute.rb
+++ app/models/post_attribute.rb
@@ -3,4 +3,11 @@ class PostAttribute < ApplicationRecord
 
   validates :attribute_name, :attribute_value, presence: true
   validates :attribute_name, :attribute_value, length: {minimum: 2, maximum: 50}
+
+  before_save :uppercase_fields
+
+  def uppercase_fields
+  	self.attribute_name = attribute_name.upcase
+  	self.attribute_value = attribute_value.upcase
+  end
 end
